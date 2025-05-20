3ff42f56d8067136fdc14b2a4ce8f58928f640a7
SathwikPedapati <161137317+sathwikpedapati@users.noreply.github.com>
2025-05-20 21:07:46 +0530

    Update Login.js

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -27,7 +27,7 @@ const Login = () => {
       return handleError('All fields are required ')
     }
     try {
-      const url = 'http://localhost:8080/auth/login'
+      const url = 'https://quiz-hub-1zsr.onrender.com/auth/login'
       const response = await fetch(url, {
         method: 'POST',
         headers: {
