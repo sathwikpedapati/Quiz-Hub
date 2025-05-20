b6ac065897e0ee39f88e3ece060a1650a310dadb
SathwikPedapati <161137317+sathwikpedapati@users.noreply.github.com>
2025-05-20 21:07:18 +0530

    Update Signup.js

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -26,7 +26,7 @@ const Signup = () => {
       return handleError('All fields are required')
     }
     try {
-      const url = 'http://localhost:8080/auth/signup'
+      const url = 'https://quiz-hub-1zsr.onrender.com/auth/signup'
       const response = await fetch(url, {
         method: 'POST',
         headers: {
